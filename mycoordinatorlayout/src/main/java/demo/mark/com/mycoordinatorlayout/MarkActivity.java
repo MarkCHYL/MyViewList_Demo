8cedc646890d403fa2f306bec39faf007f863ec1
Mark <2285581945@qq.com>
2017-10-24 09:18:15 +0800

    Mark

---
+++ mycoordinatorlayout/src/main/java/demo/mark/com/mycoordinatorlayout/MarkActivity.java
@@ -0,0 +1,13 @@
+package demo.mark.com.mycoordinatorlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mark);
+    }
+}
