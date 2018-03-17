a66686b846d3cc54e03078e1409f357efaa2580d
Mark <2285581945@qq.com>
2018-03-17 21:16:27 +0800

    Mark
    Bugly热更新的集成    第一次   只是集成了版本更新，后台能强制更新，能改变弹框的样式

---
+++ mytabledemo/src/main/java/com/mark/exe/mytabledemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mark.exe.mytabledemo;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mark.exe.mytabledemo.view.MyChart;
+
+public class MainActivity extends AppCompatActivity {
+    private MyChart myChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myChart=(MyChart)findViewById(R.id.myChart);
+
+        int[][] columnInfo=new int[][]{
+                {6, Color.BLUE},
+                {5, Color.GREEN},
+                {9, Color.RED},
+                {2, Color.GRAY},
+                {1, Color.MAGENTA},
+                {3, Color.CYAN},
+                {8, Color.YELLOW},
+        };
+
+        myChart.setColumnInfo(columnInfo);
+        myChart.setXAxisiScaleValue(100,9);
+        myChart.setYAxisiScaleValue(10,7);
+    }
+}
