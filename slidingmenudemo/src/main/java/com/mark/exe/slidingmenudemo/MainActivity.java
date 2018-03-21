d033e9d35f7c8d4821e4f8aea8882ea0fb291ab1
Mark <2285581945@qq.com>
2018-03-21 20:09:52 +0800

    Mark
    SlideMenuDemo的简历和提交完成，使用的是https://github.com/jfeinstein10/SlidingMenu，使用方法与一般的moudle导入lib一样

--- slidingmenudemo/src/main/java/com/mark/exe/slidingmenudemo/MainActivity.java
+++ slidingmenudemo/src/main/java/com/mark/exe/slidingmenudemo/MainActivity.java
@@ -2,12 +2,37 @@ package com.mark.exe.slidingmenudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SlidingMenu mSlidingMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        mSlidingMenu = new SlidingMenu(this);
+        mSlidingMenu.setMode(SlidingMenu.RIGHT);     //设置从左弹出/滑出SlidingMenu
+        mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);   //设置占满屏幕
+        mSlidingMenu.attachToActivity(this,SlidingMenu.SLIDING_CONTENT);    //绑定到哪一个Activity对象
+        mSlidingMenu.setMenu(R.layout.slidingmenulayout);                   //设置弹出的SlidingMenu的布局文件
+        mSlidingMenu.setBehindOffsetRes(R.dimen.sliding_menu_offset);       //设置SlidingMenu所占的偏移
+    }
+
+    //按下手机的Menu键，将会弹出SlidingMenu，取代传统的Menu界面
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode){
+            case KeyEvent.KEYCODE_MENU:
+                mSlidingMenu.toggle(true);
+                break;
+            default:
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
